17ff03b9ba1cb3711b5b406ae646a9344ddcb34b
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-04-14 21:37:25 +1000

    solution 438

---
+++ 438.java
@@ -0,0 +1,30 @@
+class Solution {
+    public List<Integer> findAnagrams(String s, String p) {
+        ArrayList<Integer> result = new ArrayList<Integer>();
+        char pArr[] = p.toCharArray();
+        char sArr[] = s.toCharArray();
+        if (pArr.length > sArr.length) return result;
+
+        int[] pArrCount = new int[26];
+        for (char c : pArr) {
+            pArrCount[c-'a']++;
+        }
+
+        for (int i = 0; i < sArr.length-pArr.length+1; i++) {
+            char[] checkArr = new char[pArr.length];
+            for (int m = 0; m < pArr.length; m++) {
+                checkArr[m] = sArr[i+m];
+            }
+
+            int[] checkArrMap = new int[26];
+            for (char c : checkArr) {
+                checkArrMap[c-'a']++;
+            }
+
+            if (Arrays.equals(pArrCount, checkArrMap)) {
+                result.add(i);
+            }
+        }
+        return result;
+    }
+}
