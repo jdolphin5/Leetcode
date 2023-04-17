48d8563cc942e1c3a479967a7971eb9f4cf4eb40
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-04-17 15:01:59 +1000

    solution 394

---
+++ 394.java
@@ -0,0 +1,37 @@
+class Solution {
+
+    public String decodeString(String s) {
+
+        Stack<Integer> counts = new Stack<Integer>();
+        Stack<String> result = new Stack<String>();
+        String res = "";
+        int i = 0;
+
+        while(i < s.length()) {
+            if (Character.isDigit(s.charAt(i))) {
+                int a = 0;
+                while (Character.isDigit(s.charAt(i))) {
+                    a = a * 10 + (s.charAt(i) - '0');
+                    i++;
+                }
+                counts.push(a);
+            } else if (s.charAt(i) == '[') {
+                result.push(res);
+                res = "";
+                i++;
+            } else if (s.charAt(i) == ']') {
+                String temp = result.pop();
+                int repeat = counts.pop();
+                System.out.println(temp);
+                temp += res.repeat(repeat);
+                res = temp;
+                i++;
+            } else { //letter
+                res += s.charAt(i);
+                i++;
+            }
+        }
+        return res;
+
+    }
+}
