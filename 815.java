cb2a61b253f8a3135c8894f9f77f703db48b46eb
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-04-28 20:14:43 +1000

    solution 815

---
+++ 815.java
@@ -0,0 +1,86 @@
+class Pair{
+    int node,lev;
+    Pair(int node,int lev)
+    {
+        this.node=node;
+        this.lev=lev;
+    }
+}
+class Solution {
+    public int numBusesToDestination(int[][] routes, int source, int target) {
+        if(source==target)
+            return 0;
+        HashMap<Integer,Set<Integer>> map = new HashMap<>();
+        int res = 0;
+        Queue<Pair> q = new LinkedList<>();
+        Set<Integer> vis_stops = new HashSet<>();
+
+        for(int i = 0;i < routes.length; i++)
+        {
+            Set<Integer> temp = new HashSet<>();
+            for(int it : routes[i])
+                temp.add(it);
+            map.put(i, temp);
+        }
+        
+        HashMap<Integer,Set<Integer>> stop_bus = new HashMap<>();
+
+        for(int i = 0;i < routes.length; i++)
+        {
+            for(int it : routes[i])
+            {
+                if(stop_bus.containsKey(it)){
+                    Set<Integer> temp  = new HashSet<>();
+                    for(int itr : stop_bus.get(it))
+                        temp.add(itr);
+                    temp.add(i);
+                    stop_bus.put(it,temp);
+                }
+                else{
+                    Set<Integer> temp =n ew HashSet<>();
+                    temp.add(i);
+                    stop_bus.put(it, temp);
+                }
+            }
+        }
+        
+
+        q.add(new Pair(source,1));
+        while(!q.isEmpty())
+        {
+            int node = q.peek().node;
+            int lev = q.peek().lev;
+            q.remove();
+            Set<Integer> vis_bus = new HashSet<>();
+
+            for(int bus: stop_bus.get(node))
+            {
+                if((map.get(bus)).contains(target))
+                    return lev;
+            
+                else
+                {
+                    if(!vis_stops.contains(bus))
+                    
+                    {
+                        for(int it: map.get(bus))
+                        {
+                            if(!vis_bus.contains(it)){
+                                vis_bus.add(it);
+                                q.add(new Pair(it, lev + 1));
+                            
+                            }
+
+                        }
+                    }
+
+                    vis_stops.add(bus);
+                    
+                }
+            }
+
+        }
+        return -1;
+
+    }
+}
