504378b175540d5564a5e30dd6aabd9631485b97
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-04-29 11:03:46 +1000

    solution 322

---
+++ 322.java
@@ -0,0 +1,38 @@
+class Solution {
+    int[] coins;
+    Integer[] cache;
+
+    private int helper(int amount) {
+        if (amount == 0) return 0;
+        if (amount < 0) return -1;
+
+        if (cache[amount] != null) return cache[amount];
+
+        int minCoinsChange = Integer.MAX_VALUE;
+
+        for (int num : coins) {
+            int curMinCoinsChange = helper(amount - num);
+            if (curMinCoinsChange != -1) {
+                minCoinsChange = Math.min(curMinCoinsChange, minCoinsChange);
+            }
+        }
+
+        if (minCoinsChange == Integer.MAX_VALUE) {
+            minCoinsChange = -1;
+        }
+        else {
+            minCoinsChange += 1;
+        }
+
+        cache[amount] = minCoinsChange;
+        return cache[amount];
+
+    }
+
+    public int coinChange(int[] coins, int amount) {
+        this.coins = coins;
+        this.cache = new Integer[amount + 1];
+        return helper(amount);
+    }
+
+}
