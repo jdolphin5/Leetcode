b5fc593856c5617a43a3e29d20e7ad2a54f40061
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-04-30 17:40:49 +1000

    spacing upate

--- 152.java
+++ 152.java
@@ -1,12 +1,10 @@
 class Solution {
     public int maxProduct(int[] nums) {
-
         if (nums.length == 1) return nums[0];
 
         int max = Integer.MIN_VALUE;
-
-
         int curProd = 0;
+        
         for (int i = 0; i < nums.length; i++) {
             curProd = nums[i];
             max = Math.max(curProd, max);
