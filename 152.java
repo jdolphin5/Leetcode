0c5cf9abbe15ad6d7e60b143166461dd13df4e58
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-04-30 17:35:24 +1000

    solution 152

---
+++ 152.java
@@ -0,0 +1,21 @@
+class Solution {
+    public int maxProduct(int[] nums) {
+
+        if (nums.length == 1) return nums[0];
+
+        int max = Integer.MIN_VALUE;
+
+
+        int curProd = 0;
+        for (int i = 0; i < nums.length; i++) {
+            curProd = nums[i];
+            max = Math.max(curProd, max);
+            for (int j = i+1; j < nums.length; j++) {
+                curProd = curProd * nums[j];
+                max = Math.max(curProd, max);
+            }
+        }
+
+        return max;
+    }
+}
