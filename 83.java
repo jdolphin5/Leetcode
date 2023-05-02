7b5ea32089cae11dda863dff353ec422ae7bdeb9
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-05-02 14:20:14 +1000

    solution 83

---
+++ 83.java
@@ -0,0 +1,28 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode deleteDuplicates(ListNode head) {
+        List<Integer> myList = new ArrayList();
+        ListNode cur = head;
+        if (cur == null) return head;
+        if (cur.next == null) return head;
+
+        while (cur != null) {
+            myList.add(cur.val);
+            while (cur.next != null && myList.contains(cur.next.val)) {
+                cur.next = cur.next.next;
+            }
+            cur = cur.next;
+        }
+
+        return head;
+    }
+}
