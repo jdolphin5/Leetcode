8f4c0ad27bd914d98c6384244244a5a62599cf01
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-05-11 17:35:22 +1000

    spacing

--- 2523.java
+++ 2523.java
@@ -2,7 +2,7 @@ class Solution {
     private static boolean[] sieve(int n){
         boolean prime[] = new boolean[n + 1];
         for (int i = 2; i <= n; i++) prime[i] = true;
-        for (int p=2; p*p <= n; p++) {
+        for (int p = 2; p*p <= n; p++) {
             if (prime[p] == true) {
                 for (int i = p * p; i <= n; i += p)
                     prime[i] = false;
