5cd265769441fcfc93279fbb16f88b3e08de637a
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-05-17 11:05:13 +1000

    solution 138

---
+++ 138.java
@@ -0,0 +1,33 @@
+/*
+// Definition for a Node.
+class Node {
+    int val;
+    Node next;
+    Node random;
+
+    public Node(int val) {
+        this.val = val;
+        this.next = null;
+        this.random = null;
+    }
+}
+*/
+
+class Solution {
+    Map<Node, Node> myMap = new HashMap<>();
+
+    public Node copyRandomList(Node head) {
+        if (head == null) return null;
+
+        if (myMap.containsKey(head)) {
+            return myMap.get(head);
+        }
+
+        Node node = new Node(head.val);
+        myMap.put(head, node);
+
+        node.next = copyRandomList(head.next);
+        node.random = copyRandomList(head.random);
+        return node;
+    }
+}
