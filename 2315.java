143065edba99ae2e281413fa69606d9ec362210d
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-05-17 10:06:55 +1000

    solution 2315

---
+++ 2315.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countAsterisks(String s) {
+        char[] sArr = s.toCharArray();
+        int pairCount = 0;
+        int res = 0;
+
+        for (char c : sArr) {
+            if (c == '|') {
+                pairCount++;
+            }
+            if (c == '*' && pairCount % 2 == 0) {
+                res++;
+            }
+        }
+
+        return res;
+    }
+}
