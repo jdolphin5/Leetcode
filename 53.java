17c362229fd2785822218b14f6174603235d3b1c
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-05-18 11:05:18 +1000

    solution 53

---
+++ 53.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int maxSubArray(int[] nums) {
+        int i = 0;
+        int sum = 0;
+        int max = Integer.MIN_VALUE;
+        while (i < nums.length) {
+            sum += nums[i];
+            max = Math.max(max, sum);
+            if (sum < 0) {
+                sum = 0;
+            }
+            i++;
+        }
+
+        return max;
+    }
+}
