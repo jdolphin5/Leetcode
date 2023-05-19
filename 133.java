bb4e10c149e8f13affd341f3047ddd4072341433
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-05-19 13:31:51 +1000

    solution 133

---
+++ 133.java
@@ -0,0 +1,45 @@
+/*
+// Definition for a Node.
+class Node {
+    public int val;
+    public List<Node> neighbors;
+    public Node() {
+        val = 0;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val) {
+        val = _val;
+        neighbors = new ArrayList<Node>();
+    }
+    public Node(int _val, ArrayList<Node> _neighbors) {
+        val = _val;
+        neighbors = _neighbors;
+    }
+}
+*/
+
+class Solution {
+
+    private Node dfs(Map<Integer, Node> myMap, Node node) {
+        if (myMap.containsKey(node.val)) {
+            return myMap.get(node.val);
+        }
+
+        Node newNode = new Node(node.val);
+        myMap.put(node.val, newNode);
+        for (Node n : node.neighbors) {
+            newNode.neighbors.add(dfs(myMap, n));
+        }
+
+        return newNode;
+    }
+
+    public Node cloneGraph(Node node) {
+        if (node == null) return null;
+
+        Map<Integer, Node> myMap = new HashMap<>();
+
+        return dfs(myMap, node);
+
+    }
+}
