e9ef6fa4a92aa2a14afff0f0848fcb397aba194e
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-05-20 13:13:33 +1000

    rename dp Bool array

--- 139.java
+++ 139.java
@@ -1,22 +1,22 @@
 class Solution {
-    private boolean recur(String s, List<String> wordDict, Boolean[] visited, int i) {
-        if (i == visited.length) return true;
+    private boolean recur(String s, List<String> wordDict, Boolean[] dp, int i) {
+        if (i == dp.length) return true;
 
-        if (visited[i] != null) return visited[i];
+        if (dp[i] != null) return dp[i];
 
-        for (int k = i+1; k <= visited.length; k++) {
-            if (wordDict.contains(s.substring(i, k)) && recur(s, wordDict, visited, k)) {
-                return visited[i] = true;
+        for (int k = i+1; k <= dp.length; k++) {
+            if (wordDict.contains(s.substring(i, k)) && recur(s, wordDict, dp, k)) {
+                return dp[i] = true;
             }
         }
-        return visited[i] = false;
+        return dp[i] = false;
 
     }
 
     public boolean wordBreak(String s, List<String> wordDict) {
-        Boolean[] visited = new Boolean[s.length()]; //Boolean object to contain nulls initially
+        Boolean[] dp = new Boolean[s.length()]; //Boolean object to contain nulls initially
 
-        return recur(s, wordDict, visited, 0);
+        return recur(s, wordDict, dp, 0);
 
     }
 }
