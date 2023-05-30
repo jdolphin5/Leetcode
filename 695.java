8b3e97c4611afa08729fd6c261bb4bcbde118c72
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-05-30 11:21:34 +1000

    solution 695

---
+++ 695.java
@@ -0,0 +1,27 @@
+class Solution {
+
+    private int BFS(int[][] grid, int i, int j, int area) {
+        if (i < 0 || i >= grid.length || j < 0 || j >= grid[i].length || grid[i][j] == 0) return area;
+        grid[i][j] = 0;
+        area++;
+        area = BFS(grid, i-1, j, area);
+        area = BFS(grid, i+1, j, area);
+        area = BFS(grid, i, j-1, area);
+        area = BFS(grid, i, j+1, area);
+        return area;
+    }
+
+    public int maxAreaOfIsland(int[][] grid) {
+        int max = 0;
+        int area = 0;
+
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[i].length; j++) {
+                if (grid[i][j] == 1) {
+                    max = Math.max(max, BFS(grid, i, j, area));
+                }
+            }
+        }
+        return max;
+    }
+}
