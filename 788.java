89f047afc561e14c771ddb2873ac296aa4ab684a
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-06-08 17:43:24 +1000

    solution 788

---
+++ 788.java
@@ -0,0 +1,26 @@
+class Solution {
+    private boolean isGood(int a) {
+        boolean diff = false;
+        while (a > 0) {
+            int x = a % 10;
+            if (x == 3 || x == 4 || x == 7) return false;
+            if (x == 2 || x == 5 || x == 6 || x == 9) {
+                diff = true;
+            }
+            a /= 10;
+        }
+        if (diff) {
+            return true;
+        }
+        return false;
+    }
+
+    public int rotatedDigits(int n) {
+        int count = 0;
+
+        for (int i = 1; i <= n; i++) {
+            if (isGood(i)) count++;
+        }
+        return count;
+    }
+}
