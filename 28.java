a89d7b9ab8d4ec4c2d3f9874eb599598ae3b2060
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-06-15 11:58:01 +1000

    solution 28 improve

--- 28.java
+++ 28.java
@@ -1,8 +1,5 @@
 class Solution {
     public int strStr(String haystack, String needle) {
-        for (int i = 0; i < haystack.length()-needle.length()+1; i++) {
-            if (haystack.substring(i, needle.length()+i).equals(needle)) return i;
-        }
-        return -1;
+        return haystack.indexOf(needle);
     }
 }
