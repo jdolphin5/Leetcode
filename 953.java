1b52011edf629dcfe52c15d66c097e96c2896288
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-06-21 10:41:01 +1000

    solution 953

---
+++ 953.java
@@ -0,0 +1,27 @@
+class Solution {
+    public boolean isAlienSorted(String[] words, String order) {
+        for (int i = 1; i < words.length; i++) {
+            int j = 0;
+            while (j < words[i-1].length() || j < words[i].length()) {
+                int a;
+                int b;
+                if (j < words[i-1].length()) a = 1+order.indexOf(words[i-1].charAt(j));
+                else a = 0;
+                if (j < words[i].length()) b = 1+order.indexOf(words[i].charAt(j));
+                else b = 0;
+                if (b > a) {
+                    // good
+                    break;
+                }
+                else if (b == a) {
+                    // continue
+                    j++;
+                } 
+                else { 
+                    return false;
+                }
+            }
+        }
+        return true;
+    }
+}
