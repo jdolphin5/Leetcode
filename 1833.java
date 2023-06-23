70f809a588c97ac53257ae2068fb66d06d08840b
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-06-23 20:07:10 +1000

    solution 1833

---
+++ 1833.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxIceCream(int[] costs, int coins) {
+        Arrays.sort(costs);
+        int count = 0;
+        for (int i = 0; i < costs.length; i++) {
+            if (coins >= costs[i]) {
+                coins -= costs[i];
+                count++;
+            }
+            else {
+                break;
+            }
+        }
+        return count;
+    }
+}
