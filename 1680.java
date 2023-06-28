3bb481704e9e1899884a97542e192f73c294e6f3
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-06-28 10:02:52 +1000

    solution 1680

---
+++ 1680.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int concatenatedBinary(int n) {
+        final int MOD = 1000000007;
+        long ret = 0;
+        int binaryDigits = 0; //number of binary digits in cur number
+        for (int i = 1; i <= n; i++) {
+            if ((i & (i - 1)) == 0) {
+                binaryDigits++;
+            }
+            ret = ((ret << binaryDigits) + i) % MOD; //bitshift the last result by number of binary digits then append cur number to end (i)
+        }
+        return (int)ret;
+    }
+}
