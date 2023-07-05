680186c32fa7f6a2f525e0ad195550c6cd237d09
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-07-05 19:28:42 +1000

    solution 2635 js

---
+++ 2635.js
@@ -0,0 +1,12 @@
+/**
+ * @param {number[]} arr
+ * @param {Function} fn
+ * @return {number[]}
+ */
+var map = function(arr, fn) {
+    const ret = new Array(arr.length)
+    for (let i = 0; i < arr.length; i++) {
+        ret[i] = fn(arr[i], i);
+    }
+    return ret
+};
