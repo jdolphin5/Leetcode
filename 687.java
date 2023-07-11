205f9501c74e94eafb6a9a1f1f43dede149921e7
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-07-11 16:19:52 +1000

    solution 687

---
+++ 687.java
@@ -0,0 +1,51 @@
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    int max = 0;
+    private void dfs1(TreeNode root, int len) {
+        if (root == null) return;
+        dfs1(root.left, 0);
+        dfs1(root.right, 0);
+        int left = 0;
+        int right = 0;
+        if (root.left != null) {
+            left = dfs2(root.left, 0, root.val);
+        }
+        if (root.right != null) {
+            right = dfs2(root.right, 0, root.val);
+        }
+        max = Math.max(max, left + right);
+    }
+
+    private int dfs2(TreeNode root, int len, int curVal) {
+        if (root.val == curVal) {
+            int left = 0;
+            int right = 0;
+            if (root.left != null) 
+                left = dfs2(root.left, len, curVal);
+            if (root.right != null) 
+                right = dfs2(root.right, len, curVal);
+            return 1 + Math.max(left, right);
+        }
+        return 0;
+    }
+
+    public int longestUnivaluePath(TreeNode root) {
+        if (root == null) return 0;
+        dfs1(root, 0);
+        return max;
+    }
+}
