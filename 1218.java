07808c5a063055a3b67e13f1ee16f149d8303e62
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-07-14 10:55:18 +1000

    spacing

--- 1218.java
+++ 1218.java
@@ -5,10 +5,10 @@ class Solution {
 
         for (int i = 0; i < arr.length; i++) 
         {
-            if(dp.containsKey(arr[i] - difference)){
+            if (dp.containsKey(arr[i] - difference)) {
                 dp.put(arr[i], dp.get(arr[i] - difference) + 1);
-            }else{
-                dp.put(arr[i],1);
+            } else {
+                dp.put(arr[i], 1);
             }
             max = Math.max(dp.get(arr[i]), max);
         }
