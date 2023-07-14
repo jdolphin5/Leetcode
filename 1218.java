5fe78027fcd3a05350fa6d658e11a36df3493e53
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-07-14 10:54:05 +1000

    solution 1218

---
+++ 1218.java
@@ -0,0 +1,17 @@
+class Solution {
+    public int longestSubsequence(int[] arr, int difference) {
+        int max = 0;
+        Map<Integer, Integer> dp = new HashMap<>();
+
+        for (int i = 0; i < arr.length; i++) 
+        {
+            if(dp.containsKey(arr[i] - difference)){
+                dp.put(arr[i], dp.get(arr[i] - difference) + 1);
+            }else{
+                dp.put(arr[i],1);
+            }
+            max = Math.max(dp.get(arr[i]), max);
+        }
+        return max;
+    }
+}
