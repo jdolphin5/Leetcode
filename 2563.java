a1c963b100f2309ef7f5eff5400c37d82069d160
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-08-03 21:46:56 +1000

    extend bs range instead of checking boundary value

--- 2563.java
+++ 2563.java
@@ -1,15 +1,18 @@
 class Solution {
     public long countFairPairs(int[] nums, int lower, int upper) {
-        
-        System.out.println(nums.length);
         long ret = 0;
         Arrays.sort(nums);
+
         for (int i = 0; i < nums.length-1; i++) {
+
             int left = i+1;
-            int right = nums.length-1;
+            int right = nums.length;
+
             while (left < right) {
+
                 int mid = left + (right - left) / 2;
                 int sum = nums[i] + nums[mid];
+
                 if (sum < lower) {
                     left = mid+1;
                 }
@@ -17,11 +20,13 @@ class Solution {
                     right = mid;
                 }
             }
+
             int goodLeft = left;
-            //System.out.println("i: " + i + "goodLeft: " + goodLeft);
             left = i+1;
-            right = nums.length-1;
+            right = nums.length;
+
             while (left < right) {
+
                 int mid = left + (right - left) / 2;
                 int sum = nums[i] + nums[mid];
                 if (sum > upper) {
@@ -31,12 +36,10 @@ class Solution {
                     left = mid+1;
                 }
             }
+
             int goodRight = left;
-            //System.out.println("goodRight: " + goodRight);
             ret += (goodRight - goodLeft);
-            if (nums[i] + nums[goodRight] >= lower && nums[i] + nums[goodRight] <= upper) {
-                ret++;
-            }
+
         }
         return ret;
     }
