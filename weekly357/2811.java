127b94f8caa448e1d23c31e6d5e5ba912a722f93
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-08-06 15:14:32 +1000

    cleaned up

--- weekly357/2811.java
+++ weekly357/2811.java
@@ -1,28 +1,27 @@
 class Solution {
     
-    private boolean helper(Boolean[][][] dp, List<Integer> nums, int m, int sum, int i, int j, int calls) {
+    private boolean helper(Boolean[][] dp, List<Integer> nums, int m, int sum, int i, int j) {
+        
         if (i == j) return true;
-        if (sum < m && calls > 0) return false;
-        if ((dp[i][j][0] != null) || (dp[i][j][1] != null)) {
-            return dp[i][j][0] || dp[i][j][1];
+        if (sum < m) return false;
+        
+        if ((dp[i][j] != null)) {
+            return dp[i][j];
         }
-        
-        dp[i][j][0] = helper(dp, nums, m, sum-nums.get(j), i, j-1, calls+1);
-        dp[i][j][1] = helper(dp, nums, m, sum-nums.get(i), i+1, j, calls+1);
-        
-        return dp[i][j][0] || dp[i][j][1];
-        
+    
+        return dp[i][j] =   helper(dp, nums, m, sum-nums.get(j), i, j-1) || 
+                            helper(dp, nums, m, sum-nums.get(i), i+1, j);
         
     }
     
     public boolean canSplitArray(List<Integer> nums, int m) {
-        Boolean[][][] dp = new Boolean[nums.size()][nums.size()][2];
+        Boolean[][] dp = new Boolean[nums.size()][nums.size()];
         int sum = 0;
         for (Integer num : nums) {
             sum += num;
         }
-        if (nums.size() == 1) return true;
+        if (nums.size() <= 2) return true;
         
-        return helper(dp, nums, m, sum, 0, nums.size()-1, 0);
+        return helper(dp, nums, m, sum, 0, nums.size()-1);
     }
 }
