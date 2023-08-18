30a1f54aca25722fdb714ae4bc88a66a0b62663b
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-08-18 11:38:55 +1000

    spacing

--- 1615.java
+++ 1615.java
@@ -1,17 +1,21 @@
 class Solution {
     private int countConnected(int i, int[][] roads) {
         int ret = 0;
+
         for (int j = 0; j < roads.length; j++) {
             if (roads[j][0] == i || roads[j][1] == i)
             ret++;
         }
+
         return ret;
     }
 
     public int maximalNetworkRank(int n, int[][] roads) {
         int max = 0;
         int[] connectedRoads = new int[n];
+
         Set<Pair<Integer, Integer>> roadsSet = new HashSet<>();
+
         for (int[] road : roads) {
             Pair<Integer, Integer> myPair = new Pair(road[0], road[1]);
             roadsSet.add(myPair);
@@ -24,8 +28,10 @@ class Solution {
         for (int i = 0; i < n; i++) {
             for (int j = i+1; j < n; j++) {
                 int count = 0;
+                
                 count += connectedRoads[i];
                 count += connectedRoads[j];
+
                 if (roadsSet.contains(new Pair(new Integer(i), new Integer(j))) || 
                     roadsSet.contains(new Pair(new Integer(j), new Integer(i))))
                     count--;
