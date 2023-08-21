5218d84f9797036638f4f52b8e2b231e175bee65
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-08-21 20:42:15 +1000

    line sweep solution 1943

---
+++ 1943.java
@@ -0,0 +1,34 @@
+class Solution {
+    public List<List<Long>> splitPainting(int[][] segments) {
+        List<List<Long>> ret = new ArrayList<>();
+
+        long[] paintColorChanges = new long[100002]; // 1-indexed
+        boolean[] isChange = new boolean[100002];
+
+        for (int[] seg : segments) {
+            paintColorChanges[seg[0]] += seg[2];
+            paintColorChanges[seg[1]] -= seg[2];
+            isChange[seg[0]] = true;
+            isChange[seg[1]] = true;
+        }
+
+        long sum = 0;
+        int intervalStart = 0;
+
+        for (int i = 1; i < paintColorChanges.length; i++) {
+            if (isChange[i] && sum > 0) {
+                List<Long> myList = new ArrayList<>();
+                myList.add((long)intervalStart);
+                myList.add((long)i);
+                myList.add(sum);
+                ret.add(myList);
+            }
+            if (isChange[i]) {
+                intervalStart = i;
+            }
+            sum += paintColorChanges[i];
+        }
+
+        return ret;
+    }
+}
