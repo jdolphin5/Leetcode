27d0ec8a870dacb1accd8379774a2c468729b43e
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-08-25 11:16:05 +1000

    solution redo

--- 97.java
+++ 97.java
@@ -1,26 +1,26 @@
 class Solution {
-
+    
     private boolean helper(Boolean[][] dp, String s1, String s2, String s3, int i, int j, int k) {
-        if (k == s3.length()) return true;
+        if (s3.length() == k)
+            return true;
 
-        if (dp[i][j] != null) {
+        if (dp[i][j] != null)
             return dp[i][j];
-        }
 
-        boolean ret = false;
+        boolean left = false;
+        boolean right = false;
 
-        if (i < s1.length() && s3.charAt(k) == s1.charAt(i)) {
-            ret = ret || helper(dp, s1, s2, s3, i+1, j, k+1);
-        }
-
-        if (j < s2.length() && s3.charAt(k) == s2.charAt(j)) {
-            ret = ret || helper(dp, s1, s2, s3, i, j+1, k+1);
-        }
-        return dp[i][j] = ret;
+        if (i < s1.length() && s1.charAt(i) == s3.charAt(k))
+            left = helper(dp, s1, s2, s3, i+1, j, k+1);
+        if (j < s2.length() && s2.charAt(j) == s3.charAt(k))
+            right = helper(dp, s1, s2, s3, i, j+1, k+1);
+        
+        return dp[i][j] = left || right;
     }
 
     public boolean isInterleave(String s1, String s2, String s3) {
-        if (s1.length() + s2.length() != s3.length()) return false;
+        if (s1.length() + s2.length() != s3.length())
+            return false;
 
         Boolean[][] dp = new Boolean[s1.length()+1][s2.length()+1];
 
