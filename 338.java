40037278678cedcd58f50da2b8b98385c236d711
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-09-01 10:59:06 +1000

    solution 338 redo

--- 338.java
+++ 338.java
@@ -1,15 +1,21 @@
 class Solution {
     public int[] countBits(int n) {
         int[] ans = new int[n+1];
-        for (int i = 0; i <= n; i++) {
-            int res = 0;
+        int i = 0;
+
+        while (i <= n) {
             int x = i;
-            while(x != 0){
-                res += ( x & 1);
+            int count = 0;
+
+            while (x > 0) {
+                count += x & 1;
                 x = x >> 1;
             }
-            ans[i] = res;
+
+            ans[i] = count;
+            i++;
         }
+
         return ans;
     }
 }
