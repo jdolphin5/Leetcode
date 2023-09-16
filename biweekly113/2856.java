eff516df5b2e835d2fce9a9784fce77dd4147f13
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-09-17 02:12:12 +1000

    added comment

--- biweekly113/2856.java
+++ biweekly113/2856.java
@@ -17,7 +17,7 @@ class Solution {
         for (int i = 0; i < nums.size()/2; i++) {
             int j = nums.size()/2+i;
             if (isOdd) j++;
-            if (nums.get(i) >= nums.get(j)) {
+            if (nums.get(i) >= nums.get(j)) { //== does not work as it checks pointer of Integer are equal, not the value
                 count+=2;
             }
             //System.out.println(nums.get(i) + " : " + nums.get(j));
