9f4a11c30c439ad8ba694a90c21f90dfbbbe707f
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-09-17 14:36:35 +1000

    comment added

--- weekly363/2861.java
+++ weekly363/2861.java
@@ -1,14 +1,16 @@
 class Solution {
     private boolean canMake(int n, int k, int budget, List<List<Integer>> composition, List<Integer> stock, List<Integer> cost, long mid) {
         boolean good = false;
+
         for (int j = 0; j < composition.size(); j++) {
             long needToPay = 0;
+
             for (int i = 0; i < stock.size(); i++) {
                 long needToBuy = (mid*composition.get(j).get(i))-stock.get(i);
                 if (needToBuy > 0)
                     needToPay += needToBuy * cost.get(i);
             }
-            //System.out.println("mid: " + mid + " needToPay: " + needToPay);
+
             if (needToPay <= budget) {
                 return true;
             }
@@ -19,10 +21,11 @@ class Solution {
     
     public int maxNumberOfAlloys(int n, int k, int budget, List<List<Integer>> composition, List<Integer> stock, List<Integer> cost) {
         long lower = 0;
-        long upper = 200000000;
+        long upper = 200000000; // maximum possible budget + maximum possible stock (if composition values are all 1)
         
         while (lower < upper) {
             long mid = lower + (upper - lower) / 2;
+            
             if (canMake(n, k, budget, composition, stock, cost, mid)) {
                 lower = mid+1;
             }
