9ef508396e15f8a16584107926a4c9c07599d549
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2023-11-16 12:05:30 +1100

    solution 1980

---
+++ 1980.java
@@ -0,0 +1,32 @@
+class Solution {
+    String ret = "";
+
+    private void recur(Set<String> numsSet, int i, int n, String cur) {
+        if (i == n) {
+            if (!numsSet.contains(cur)) {
+                ret = cur;
+            }
+
+            return;
+        }
+
+        String curCopy1 = cur + "0";
+        String curCopy2 = cur + "1";
+
+        recur(numsSet, i+1, n, curCopy1);
+        recur(numsSet, i+1, n, curCopy2);
+    }
+
+    public String findDifferentBinaryString(String[] nums) {
+        Set<String> numsSet = new HashSet<>();
+        int n = nums[0].length();
+
+        for (String s : nums) {
+            numsSet.add(s);
+        }
+
+        recur(numsSet, 0, n, "");
+
+        return ret;
+    }
+}
