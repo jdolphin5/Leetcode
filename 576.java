77a0287a3630de84b3861976dc16eeba5345bda0
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-01-26 11:31:12 +1100

    clean up

--- 576.java
+++ 576.java
@@ -1,34 +1,37 @@
 class Solution {
-
-    private int helper(int[][][] dp, int m, int n, int remainingMoves, int row, int col, final int MOD) {
-        if (row >= m || row < 0 || col >= n || col < 0) {
+    private int recur(Integer[][][] dp, int m, int n, int moves, int i, int j) {
+        int ret = 0;
+        final int MOD = 1000000007;
+        
+        if (i < 0 || i >= m || j < 0 || j >= n) {
             return 1;
         }
-        if (remainingMoves == 0) {
+
+        if (dp[i][j][moves] != null) {
+            return dp[i][j][moves];
+        }
+
+        if (moves == 0) {
             return 0;
         }
-        if (dp[row][col][remainingMoves] != -1) {
-            return dp[row][col][remainingMoves];
-        }
 
-        int left = helper(dp, m, n, remainingMoves-1, row, col-1, MOD);
-        int right = helper(dp, m, n, remainingMoves-1, row, col+1, MOD);
-        int up = helper(dp, m, n, remainingMoves-1, row-1, col, MOD);
-        int down = helper(dp, m, n, remainingMoves-1, row+1, col, MOD);
+        ret += recur(dp, m, n, moves - 1, i - 1, j);
+        ret %= MOD;
+        ret += recur(dp, m, n, moves - 1, i + 1, j);
+        ret %= MOD;
+        ret += recur(dp, m, n, moves - 1, i, j - 1);
+        ret %= MOD;
+        ret += recur(dp, m, n, moves - 1, i, j + 1);
+        ret %= MOD;
 
-        return dp[row][col][remainingMoves] = (int)(((long)left + right + down + up + MOD) % MOD);
+        return dp[i][j][moves] = ret;
     }
 
     public int findPaths(int m, int n, int maxMove, int startRow, int startColumn) {
+        Integer[][][] dp = new Integer[m][n][maxMove+1];
+        
         final int MOD = 1000000007;
-        int[][][] dp = new int[m][n][51];
-        for (int[][] arr1 : dp) {
-            for (int[] arr2 : arr1) {
-                Arrays.fill(arr2, -1);
-            }
-        }
 
-        return helper(dp, m, n, maxMove, startRow, startColumn, MOD);
-
+        return recur(dp, m, n, maxMove, startRow, startColumn) % MOD;
     }
 }
