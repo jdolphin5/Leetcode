9a4dc6317330057ec70e8acb737b0a4e144512e3
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-01-30 21:57:45 +1100

    solution 150 - redo

--- 150.java
+++ 150.java
@@ -1,53 +1,33 @@
 class Solution {
     public int evalRPN(String[] tokens) {
-        Stack<Integer> stack = new Stack<>();
+        Stack<Integer> myStack = new Stack<>();
 
         for (String s : tokens) {
-
-            char[] cArr = s.toCharArray();
-            boolean isNumeric = true;
-            for (int i = 0; i < cArr.length; i++) {
-                if (!Character.isDigit(cArr[i])) {
-                    if (i == 0 && cArr.length > 1) {
-                        break;
-                    }
-                    isNumeric = false;
-                    break;
-                }
+            if (s.equals("+")) {
+                int a = myStack.pop();
+                int b = myStack.pop();
+                myStack.push(a+b);
             }
-
-            if (isNumeric) {
-                Integer x = Integer.parseInt(s);
-                stack.push(x);
+            else if (s.equals("-")) {
+                int a = myStack.pop();
+                int b = myStack.pop();
+                myStack.push(b-a);
+            }
+            else if (s.equals("*")) {
+                int a = myStack.pop();
+                int b = myStack.pop();
+                myStack.push(a*b);
+            }
+            else if (s.equals("/")) {
+                int a = myStack.pop();
+                int b = myStack.pop();
+                myStack.push(b/a);
             }
             else {
-                if (stack.size() < 2) return 0;
-                if (s.equals("+") || s.equals("-")) {
-                    int a = stack.pop();
-                    if (s.equals("-")) 
-                        a = a * -1;
-                    int b = stack.pop();
-                    int sum = a + b;
-                    stack.push(sum);
-                }
-                else if (s.equals("*")) {
-                    Integer a = stack.pop();
-                    Integer b = stack.pop();
-                    stack.push(a*b);
-                }
-                else if (s.equals("/")) {
-                    Integer a = stack.pop();
-                    Integer b = stack.pop();
-                    stack.push(b/a);
-                }
+                myStack.push(Integer.parseInt(s));
             }
-
         }
 
-        if (!stack.isEmpty()) {
-            return stack.pop();
-        }
-        return 0;
-
+        return myStack.pop();
     }
 }
