f2eb24b35155246fb8f203bfc63eeafb785f0aeb
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-04-03 12:05:42 +1100

    solution 79 - cleaner

--- 79.java
+++ 79.java
@@ -1,32 +1,32 @@
 class Solution {
+    private boolean search(char[][] board, String word, int x, int i, int j, boolean[][] visited) {
+        if (x == word.length()) return true;
+        
+        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || visited[i][j] || word.charAt(x) != board[i][j])
+            return false;
 
-    private boolean wordFound(char[][] board, String word, int curChar, int i, int j, boolean[][] visited) {
-        if (i < 0 || j < 0 || i >= board.length || j >= board[0].length || visited[i][j]) return false;
+        visited[i][j] = true;
 
+        boolean ret = search(board, word, x+1, i-1, j, visited) ||
+        search(board, word, x+1, i+1, j, visited) ||
+        search(board, word, x+1, i, j-1, visited) ||
+        search(board, word, x+1, i, j+1, visited);
 
-        if (board[i][j] == word.charAt(curChar)) {
-            //System.out.println("i: " + i + " j: " + j);
-            visited[i][j] = true;
-            curChar++;
-            if (curChar == word.length()) return true;
-            if (wordFound(board, word, curChar, i+1, j, visited) ||
-                    wordFound(board, word, curChar, i-1, j, visited) ||
-                    wordFound(board, word, curChar, i, j+1, visited) ||
-                    wordFound(board, word, curChar, i, j-1, visited) )
-                return true;
-            visited[i][j] = false;
-        }
+        visited[i][j] = false;
 
-        return false;
+        return ret;
+
     }
 
     public boolean exist(char[][] board, String word) {
         for (int i = 0; i < board.length; i++) {
             for (int j = 0; j < board[0].length; j++) {
                 boolean[][] visited = new boolean[board.length][board[0].length];
-                if (wordFound(board, word, 0, i, j, visited)) return true;
+                if (search(board, word, 0, i, j, visited))
+                    return true;
             }
         }
+
         return false;
     }
 }
