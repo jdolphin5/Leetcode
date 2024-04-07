0ef6f2118f86b90523d2db3bc02bff2bc72508f4
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-04-07 10:13:42 +1000

    solution 678

---
+++ 678.java
@@ -0,0 +1,42 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        Stack<Integer> leftStack = new Stack<>();
+        Stack<Integer> asteriskStack = new Stack<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            switch(s.charAt(i)) {
+                case '(':
+                    leftStack.push(i);
+                    break;
+                case '*':
+                    asteriskStack.push(i);
+                    break;
+                case ')':
+                    if (!leftStack.isEmpty()) {
+                        leftStack.pop();
+                    }
+                    else if (!asteriskStack.isEmpty()) {
+                        asteriskStack.pop();
+                    }
+                    else {
+                        return false;
+                    }
+                    break;
+            }
+        }
+
+        if (leftStack.isEmpty())
+            return true;
+
+        if (asteriskStack.size() < leftStack.size())
+            return false;
+
+        while (leftStack.size() > 0) {
+            if (asteriskStack.pop() < leftStack.pop()) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
