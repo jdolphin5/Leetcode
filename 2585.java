3154a8f688615af8f688a8b166f10b37bd9f47f8
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-04-11 08:54:19 +1000

    solution 2585

---
+++ 2585.java
@@ -0,0 +1,38 @@
+public class Solution {
+    private int recur(int target, int[][] types, Integer[][] dp, int i) {
+        final int MOD = 1000000007;
+        
+        if (target == 0) {
+            return 1;
+        }
+        if (i == types.length) {
+            return 0;
+        }
+        if (dp[i][target] != null) {
+            return dp[i][target];
+        }
+
+        int ct = types[i][0];
+        int val = 0;
+        int ans = 0;
+
+        for (int j = 0; j <= ct; j++) {
+            if (val <= target) {
+                ans = ans + (recur(target - val, types, dp, i+1)) % MOD;
+                ans %= MOD;
+            }
+            
+            val += types[i][1];
+        }
+
+        return dp[i][target] = ans % MOD;
+    }
+
+    public int waysToReachTarget(int target, int[][] types) {
+        Integer[][] dp = new Integer[types.length][target+1];
+
+        return recur(target, types, dp, 0);
+    }
+} 2585 {
+    
+}
