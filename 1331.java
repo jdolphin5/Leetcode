ba653938e06f7a07edc52de2603df1bc96041222
jdolphin5 <65531401+jdolphin5@users.noreply.github.com>
2024-10-02 12:52:15 +1000

    solution 1331

---
+++ 1331.java
@@ -0,0 +1,42 @@
+class Solution {
+    public class Tuple {
+        int val;
+        int index;
+
+        public Tuple(int val, int index) {
+            this.val = val;
+            this.index = index;
+        }
+    }
+
+    public int[] arrayRankTransform(int[] arr) {
+        int[] copyArr = new int[arr.length];
+        
+        for (int i = 0; i < arr.length; i++) {
+            copyArr[i] = arr[i];
+        }
+
+        Arrays.sort(copyArr);
+        Map<Integer, Integer> valueToRankMap = new HashMap<>();
+        int offset = 0;
+        int last = Integer.MIN_VALUE;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (copyArr[i] == last) {
+                offset++;
+                continue;
+            }
+
+            valueToRankMap.put(copyArr[i], i+1-offset);
+            last = copyArr[i];
+        }
+
+        int[] ret = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            ret[i] = valueToRankMap.get(arr[i]);
+        }
+
+        return ret;
+    }
+}
